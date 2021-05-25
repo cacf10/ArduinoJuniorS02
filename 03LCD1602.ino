7d3605563b179594de5be1982688b25720675817
cacf10 <cacf10@163.com>
2021-05-25 15:49:06 +0800

    Add files via upload

---
+++ 03LCD1602.ino
@@ -0,0 +1,22 @@
+//sketch created by Akshay Joseph
+#include <Wire.h> 
+#include <DFRobot_LiquidCrystal_I2C.h>
+
+DFRobot_LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup()
+{
+
+  lcd.init();
+  lcd.begin(16, 2);
+
+
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(4,0);
+  lcd.print("hello! code!");
+}
+
+void loop()
+{
+}
