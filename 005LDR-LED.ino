6f631a8c6e0d47c0beeb5e5ffec0a36b44011ae5
cacf10 <cacf10@163.com>
2021-08-10 16:28:00 +0800

    Add files via upload

---
+++ 005LDR-LED.ino
@@ -0,0 +1,30 @@
+
+
+int Vin=5;        //voltage at 5V pin of arduino
+float Vout=0;     //voltage at A0 pin of arduino
+float R1=4700;    //value of known resistance
+float R2=0;       //value of unknown resistance
+int a2d_data=0;    
+float buffer=0;            
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(3,OUTPUT);
+}
+
+void loop()
+{
+  a2d_data=analogRead(A0);
+  if(a2d_data)
+  {
+    buffer=a2d_data/4;
+    analogWrite(3, buffer);
+
+    Serial.println(buffer);
+
+    
+    
+    delay(30);
+  }
+}
